a662255640221de15e9989672fca6ec8ee13dbd8
cflament <flament.cha@gmail.com>
2019-10-25 01:21:06 +0200

    bug fix

--- airbndogs/app/models/dog.rb
+++ airbndogs/app/models/dog.rb
@@ -1,4 +1,5 @@
 class Dog < ApplicationRecord
+    has_many :stalls
     has_many :dogsitters, through: :stralls
     belongs_to :city, optional: true
 end
--- airbndogs/app/models/dogsitter.rb
+++ airbndogs/app/models/dogsitter.rb
@@ -1,4 +1,5 @@
 class Dogsitter < ApplicationRecord
+    has_many :stralls
     has_many :dogs, through: :stralls
     belongs_to :city, optional: true
 end
